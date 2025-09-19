486d8c981cebf33f2235c89cee718f073bc0acb3
wenxing-l <2339410482@qq.com>
2025-09-19 08:52:43 +0800

    test-up

---
+++ try/src/practice.v
@@ -0,0 +1,43 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 2025/09/03 14:45:38
+// Design Name: 
+// Module Name: practice
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+module example(
+    input   a,
+    input   rst_n,
+    input   clk,
+    output  out
+);
+
+
+    
+endmodule
+/*
+module example(
+    input   a,
+    input   b,
+    input   cin,
+    output  cout,
+    output  sum
+);
+assign cout = (a&b)|((a^b)&cin);
+assign sum =(a^b)^cin;
+    
+endmodule
+*/
